21b2035d480a773e16f791acb8fa28648bd32f0b
Aldo Fonseca <aj.silvaf2003@gmail.com>
2024-04-24 18:05:36 -0400

    fixed and added components of the HomePage

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,13 +1,29 @@
 import "./HomePage.css";
 import  WholePie from "./HomePageAssests/WholePie.png"
+import pieslice from "./HomePageAssests/slice_of_pie.png";
+
+
 function HomePage(){
     return(
         <>
+            <div className="Welcome-page">
+
             <h1>Cook Like a chef in your own Kicthen</h1>
             <h3>Tell us what's in your fridge and we'll do the rest</h3>
 
             <h5>Already know what to make? Search for the recipe here</h5>
-            <img src={WholePie} alt="" className="rightSide-img"/>
+            </div>
+            <img src={WholePie} alt="Left Pie" className="rightSide-img"/>
+
+            <div className="scrolling-recipes">
+
+                <div className="scroll-header">
+                    <h3>Over 1850 Recipes</h3>
+                    <h1>A Recipe for every</h1>
+                    <h1>Preference and skill level</h1>
+                </div>
+            </div>
+            <img src={pieslice} alt="Pie slice" className="LeftsidePie" />
         </>
     )
 }
