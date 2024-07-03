093da66f90c5630722d2c4313064a5eb16225a27
Alto0327 <aj.silvaf2003@gmail.com>
2024-07-03 19:29:59 -0400

    test

--- src/Pages/Search Recipie/Results.jsx
+++ src/Pages/Search Recipie/Results.jsx
@@ -23,6 +23,7 @@ function Results() {
       const response = await fetch(url);
       if (!response.ok) {
         throw new Error('Network response was not ok');
+        
       }
       const data = await response.json();
       setRecipes(data);
