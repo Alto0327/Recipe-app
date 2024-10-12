8e9cd9df56c2b750044374ab29c9a23c9accda95
Alto0327 <aj.silvaf2003@gmail.com>
2024-10-12 17:14:27 -0400

    Created Recipe Card File

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -4,45 +4,9 @@ import pieslice from "./HomePageAssests/slice_of_pie.png";
 import RecipeSearch from "../../RecipeSearch";
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import RecipeCard from "./RecipeCard";
 function HomePage() {
-  const [randomRecipes, setRandomRecipes] = useState([]);
 
-  useEffect(() => {
-    const fetchRandomRecipes = async () => {
-      const APIKey = import.meta.env.VITE_API_VAR;
-      const numberOfRecipes = 15; // Number of random recipes to fetch
-      const url = `https://api.spoonacular.com/recipes/random?number=${numberOfRecipes}&apiKey=${APIKey}`;
-
-      try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        setRandomRecipes(data.recipes);
-      } catch (error) {
-        console.error('There was a problem with the fetch operation:', error);
-      }
-    };
-
-    fetchRandomRecipes();
-  }, []);
-
-// TODO: make the recipe cards into own file
-    // Use a utility function to duplicate recipe cards for continuous scrolling
-    useEffect(() => {
-        const duplicateCards = () => {
-          const container = document.getElementById('recipesContainer');
-          if (!container) return; // Ensure the container exists before proceeding
-      
-          const cards = container.getElementsByClassName('recipe-card');
-          const clone = [...cards].map(card => card.cloneNode(true));
-          clone.forEach(card => container.appendChild(card));
-        };
-      
-        duplicateCards();
-      }, [randomRecipes]);
   return (
     <>
       <div className="Welcome-page">
@@ -75,28 +39,7 @@ function HomePage() {
 </div>
 <div className="scrolling-recipes">
   <div className="recipes-container-right">
-  <div>
-    {randomRecipes.map(recipe => (
-      <div key={recipe.id} className="recipe-card">
-        <a href={recipe.sourceUrl} target="_blank">
-          <img src={recipe.image} alt={recipe.title} />
-          <h2>{recipe.title}</h2>
-          <p>Ready in {recipe.readyInMinutes} minutes</p>
-        </a>
-      </div>
-    ))}
-  </div>
-  <div>
-    {randomRecipes.map(recipe => (
-      <div key={recipe.id} className="recipe-card">
-        <a href={recipe.sourceUrl} target="_blank">
-          <img src={recipe.image} alt={recipe.title} />
-          <h2>{recipe.title}</h2>
-          <p>Ready in {recipe.readyInMinutes} minutes</p>
-        </a>
-      </div>
-    ))}
-  </div>
+    <RecipeCard />
   </div>
 
   </div>
